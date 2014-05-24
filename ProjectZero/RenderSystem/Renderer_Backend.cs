ca3022f96034b87d14c9e9185d639742a41b7fa3
Andreas Strid <andreas_strid@bredband.net>
2014-05-24 16:41:35 +0200

    Not working... Should render back to front and use diffrent sprite batches for diffrent layers.

--- ProjectZero/RenderSystem/Renderer_Backend.cs
+++ ProjectZero/RenderSystem/Renderer_Backend.cs
@@ -16,11 +16,24 @@ namespace ProjectZero.RenderSystem
 
         public void Render(GameTime gameTime)
         {
-            //_commands.Sort(new CommandComparer());
-
+            _commands.Sort(new CommandComparer());
+            SpriteBatch spriteBatch = null;
             foreach (var c in _commands)
             {
-                c.Render(this, gameTime);
+                if (spriteBatch != c.SpriteBatch)
+                {
+                    if (spriteBatch != null)
+                    {
+                        spriteBatch.End();
+                    }
+                    spriteBatch = c.SpriteBatch;
+                    spriteBatch.Begin(SpriteSortMode.FrontToBack, BlendState.AlphaBlend);
+                }
+                c.Render(this, gameTime);                
+            }
+            if (spriteBatch != null)
+            {
+                spriteBatch.End();
             }
             _commands.Clear();            
         }
@@ -34,84 +47,62 @@ namespace ProjectZero.RenderSystem
                     return 0;
                 }
 
-                ClearColorCommand clearX = x as ClearColorCommand;
-                ClearColorCommand clearY = y as ClearColorCommand;
-                if (clearX != null && clearY != null)
-                {
-                    return clearX.AddedIndex <= clearY.AddedIndex ? -1 : 1;
-                }
-
-                if (clearX != null)
-                {
-                    return clearX.AddedIndex < y.AddedIndex ? -1 : 1;
-                }
-
-                if (clearY != null)
-                {
-                    return x.AddedIndex < clearY.AddedIndex ? -1 : 1;
-                }
-
-                if (x.SortValue < y.SortValue)
+                // Clear always first.
+                if (x.SpriteBatch == null)
                 {
                     return -1;
                 }
-                else if (x.SortValue > y.SortValue)
+                if (y.SpriteBatch == null)
                 {
                     return 1;
                 }
 
+                if (x.SpriteIndex < y.SpriteIndex)
+                {
+                    return -1;
+                }
+                else if (x.SpriteIndex > y.SpriteIndex)
+                {
+                    return 1;
+                }
 
-                // same position in world, so decide based on added index.
-                if (x.AddedIndex < y.AddedIndex)
+                /*if (x.AddedIndex < y.AddedIndex)
                 {
                     return -1;
                 }
                 else if (x.AddedIndex > y.AddedIndex)
                 {
                     return 1;
-                }
-
-                return 0;                
+                }*/
+                return 0;
             }
         }
 
         private abstract class Command
         {
+            public Command(SpriteBatch spriteBatch, int spriteIndex, int addedIndex)
+            {
+                SpriteBatch = spriteBatch;
+                SpriteIndex = spriteIndex;
+                AddedIndex = addedIndex;
+            }
+
             public abstract void Render(Renderer renderer, GameTime gameTime);
 
-            public abstract int SortValue { get; }
+            public SpriteBatch SpriteBatch { get; private set; }
 
-            public abstract int AddedIndex { get; }
+            public int SpriteIndex { get; private set; }
+
+            public int AddedIndex { get; private set; }
         }
 
         private class ClearColorCommand : Command
         {
             private readonly Color _color;
-            private readonly int _addedIndex;
 
-            public ClearColorCommand(Color color, int addedIndex)
+            public ClearColorCommand(Color color) : base(null, int.MinValue, int.MinValue)
             {
                 _color = color;
-                _addedIndex = addedIndex;
-            }
-
-            public override int SortValue
-            {
-                get
-                {
-                    // always first, but in order added with all the rest of the commands.
-                    // will special case when sorting on added index.
-                    // all commands with AddedIndex > will be after.
-                    return int.MinValue;
-                }                
-            }
-
-            public override int AddedIndex
-            {
-                get
-                {
-                    return _addedIndex;
-                }
             }
 
             public override void Render(Renderer renderer, GameTime gameTime)
@@ -120,63 +111,43 @@ namespace ProjectZero.RenderSystem
             }
         }
 
+        
         private class DrawImageCommand : Command
         {
             private readonly TextureHandle _texture;
             private readonly Vector2? _position;
             private readonly Rectangle? _drawRect;
-            private readonly Rectangle? _sourceRect;
-            private readonly SpriteBatch _spriteBatch;
-            private readonly int _addedIndex;
+            private readonly Rectangle? _sourceRect;            
             // for mouse pointer and so on.
             private readonly bool _forceDrawLast;
 
-            public override int SortValue
+            private float SortValue
             {
                 get
                 {
-                    
-                    
-                    // in map order.
-                    int i = _position != null ?
-                        ((int)(_position.Value.X / Map.TileSize) + (int)(_position.Value.Y / Map.TileSize) * Map.Rows) :
-                        (_drawRect.Value.X / Map.TileSize + _drawRect.Value.Y / Map.TileSize * Map.Rows);
-                    return !_forceDrawLast ? i : (int.MaxValue - i);
+                    if (_position != null)
+                    {
+                        return (int)(_position.Value.X / Map.TileSize) + (int)(_position.Value.Y / Map.TileSize) * Map.Rows;
+                    }
+
+                    return (int)(_drawRect.Value.X / Map.TileSize) + (int)(_drawRect.Value.Y / Map.TileSize) * Map.Rows;
                 }
             }
 
-            public override int AddedIndex
-            {
-                get
-                {
-                    return _addedIndex;
-                }
-            }
-
-            public DrawImageCommand(TextureHandle texture, Vector2? position, Rectangle? drawRect, Rectangle? sourceRect, SpriteBatch spriteBatch, int addedIndex, bool forceDrawLast = false)
+            public DrawImageCommand(TextureHandle texture, Vector2? position, Rectangle? drawRect, Rectangle? sourceRect, SpriteBatch spriteBatch, int spriteIndex, int addedIndex, bool forceDrawLast = false) : base(spriteBatch, spriteIndex, addedIndex)
             {
                 _texture = texture;
                 _position = position;
                 _drawRect = drawRect;
                 _sourceRect = sourceRect;
-                _spriteBatch = spriteBatch;
-                _addedIndex = addedIndex;
-                _forceDrawLast = forceDrawLast;
+                _forceDrawLast = forceDrawLast;                
             }
 
             public override void Render(Renderer renderer, GameTime gameTime)
             {
-                // TODO:    should include sort mode, blend state.
-                _spriteBatch.Begin();
-                float layerDepth = _position != null ?
-                        (_position.Value.X / Map.TileSize) + ((_position.Value.Y / Map.TileSize) * Map.Rows) :
-                        (_drawRect.Value.X / Map.TileSize + _drawRect.Value.Y / Map.TileSize * Map.Rows);
-                if (_forceDrawLast)
-                {
-                    layerDepth = float.MaxValue;
-                }
-                _spriteBatch.Draw((Texture2D)_texture.Texture, position: _position, drawRectangle: _drawRect, sourceRectangle: _sourceRect);
-                _spriteBatch.End();
+                float depth = 1.0f - (SortValue / (Map.Columns * Map.Rows)) * 0.5f - AddedIndex * 1.0f / (Map.Columns * Map.Rows) * 0.5f;
+                //float depth = SortValue / (Map.Columns * Map.Rows);
+                SpriteBatch.Draw((Texture2D)_texture.Texture, position: _position, drawRectangle: _drawRect, sourceRectangle: _sourceRect, depth: depth);                
             }
         }
 
@@ -186,43 +157,29 @@ namespace ProjectZero.RenderSystem
             private readonly Vector2 _position;
             private readonly Color _color;
             private readonly string _text;
-            private readonly SpriteBatch _spriteBatch;
-            private readonly int _addedIndex;
 
-            public override int SortValue
-            {
-                get
-                {
-                    // in map order, but always last.
-                    int i = (int)(_position.X / Map.TileSize) + (int)(_position.Y / Map.TileSize) * Map.Rows;
-                    return int.MaxValue - i;
-                }
-            }
-
-            public override int AddedIndex
-            {
-                get
-                {
-                    return _addedIndex;
-                }
-            }
-
-            public DrawStringCommand(FontHandle font, Vector2 position, Color color, string text, SpriteBatch spriteBatch, int addedIndex)
+            public DrawStringCommand(FontHandle font, Vector2 position, Color color, string text, SpriteBatch spriteBatch, int spriteIndex, int addedIndex) : base(spriteBatch, spriteIndex, addedIndex)
             {
                 _font = font;
                 _position = position;
                 _color = color;
-                _text = text;
-                _spriteBatch = spriteBatch;
-                _addedIndex = addedIndex;
+                _text = text;            
+            }
+
+
+            private float SortValue
+            {
+                get
+                {
+                    float totalGridCells = Map.Rows * Map.Columns;
+
+                    return (_position.X / Map.TileSize) + (_position.Y / Map.TileSize * Map.Rows);// + totalGridCells * _addedIndex;                    
+                }
             }
 
             public override void Render(Renderer renderer, GameTime gameTime)
             {
-                // TODO:    should include sort mode, blend state.
-                _spriteBatch.Begin();// SpriteSortMode.Immediate, BlendState.AlphaBlend);
-                _spriteBatch.DrawString(_font.Font, _text, _position, _color);
-                _spriteBatch.End();
+                SpriteBatch.DrawString(_font.Font, _text, _position, _color);                
             }
         }
     }
